1eedd5932312132267e22dd6a2db10eb856a73c7
G. motta <81563202+lambdodelta@users.noreply.github.com>
2022-03-10 15:07:25 +0100

    first push from PC

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const flat = React.useRef()
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  function render() {
+    return (
+      <View>
+        <Animated.FlatList
+        ref={flat}
+        horizontal
+        pagingEnabled
+        snapToAlignment="center"
+        snapToInterval="40px"
+        decelerationRate="fast"
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>lol</Text>
+            </View>
+          )
+        }}
+        keyExtractor
+        onScroll={
+          Animated.event([
+            { nativeEvent: {contentOffset: {x: scrollX}} }
+          ],
+          {
+            useNativeDriver: false
+          })
+        }>
+        </Animated.FlatList>
+      </View>
+    )
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
